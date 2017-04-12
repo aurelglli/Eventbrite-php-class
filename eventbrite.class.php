36363087aed634ce61cb93dd516e34181ee27506
aurelglli <aureliengirelli@gmail.com>
2017-04-12 17:55:54 +0200

    Update eventbrite.class.php

--- eventbrite.class.php
+++ eventbrite.class.php
@@ -1,6 +1,6 @@
 <?php
 /*
-    Copyright 2017 Aurélien Girelli (aka aurelglli) aurelien@daylug.co
+    Copyright 2017 Aurélien Girelli (aka aurelglli) aurelien@daylug.fr
     Licensed under the Apache License, Version 2.0 (the "License"); you may not
     use this file except in compliance with the License. You may obtain a copy of
     the License at
